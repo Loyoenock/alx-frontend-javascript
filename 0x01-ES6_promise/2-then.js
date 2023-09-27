e2c6f086434c8da63a93ad67171e9a2136808f0b
Loyoenock <loyoenock@gmail.com>
2023-09-27 02:06:34 +0000

    Appending handlers to a function

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then((response) => {
-      // When the Promise resolves, return an object with status: 200 and body: 'success'.
-      return { status: 200, body: 'success' };
-    })
-    .catch((error) => {
-      // When the Promise rejects, return an empty Error object.
-      return new Error();
-    })
-    .then(() => {
-      // Log 'Got a response from the API' for every resolution.
-      console.log('Got a response from the API');
-    });
+  // Attach handlers to the provided Promise.
+  return promise
+    .then(() => ({ status: 200, body: 'success' })) // When the Promise resolves, return an object with status: 200 and body: 'success'.
+    .catch(() => new Error()) // When the Promise rejects, return an empty Error object.
+    .finally(() => console.log('Got a response from the API')); // Regardless of resolution or rejection, log 'Got a response from the API'.
 }

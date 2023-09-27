92a9cb65bd71e18d04a70ecf585730e643ddc3ff
Loyoenock <loyoenock@gmail.com>
2023-09-27 02:47:21 +0000

    Eslint style adjustmet

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,4 +1,4 @@
-// Export a function named loadBalancer that uses Promise.race to prioritize between China and US downloads.
+// Export a function named "loadBalancer" that uses Promise.race to prioritize between China and US downloads.
 export default function loadBalancer(chinaDownload, USDownload) {
   // Return a Promise that resolves or rejects based on which download completes first.
   return Promise.race([chinaDownload, USDownload]);

45782de38a19061beedc27ce54e67a195f945c5d
Loyoenock <loyoenock@gmail.com>
2023-09-27 02:42:41 +0000

    export a function named loadBalancer. It should accept two arguments chinaDownload (Promise) and USDownload (Promise).

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,5 @@
+// Export a function named loadBalancer that uses Promise.race to prioritize between China and US downloads.
+export default function loadBalancer(chinaDownload, USDownload) {
+  // Return a Promise that resolves or rejects based on which download completes first.
+  return Promise.race([chinaDownload, USDownload]);
+}

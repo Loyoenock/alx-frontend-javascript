d1d9a25ca76525a204c2f6859cfade9891f01f04
Loyoenock <loyoenock@gmail.com>
2023-10-03 13:12:01 +0000

    A function getStudentsByLocation that returns an array of objects who are located in a specific city

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,3 @@
 export default function getStudentsByLocation(students, city) {
-  if (!Array.isArray(students) || typeof city !== 'string') {
-    return [];
-  }
-
-  return students.filter(student => student.location === city);
+  return students.filter((obj) => obj.location === city);
 }
